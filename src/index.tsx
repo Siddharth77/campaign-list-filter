f0580c3952f4f8549695d6b11accadd27bfa1a7a
Siddharth Jain <siddharth.jain02@nagarro.com>
2022-06-24 19:40:06 +0530

    Assignment: Refactor code

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { store } from "./app/store";
+import CampaignList from "./pages/home/CampaignList";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <CampaignList />
   </Provider>
 );
 
