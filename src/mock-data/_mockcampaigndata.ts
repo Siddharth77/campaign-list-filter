9f16be75e2d80da5fdfc51adc5564d6e8b7b323c
Siddharth Jain <siddharth.jain02@nagarro.com>
2022-06-28 19:38:52 +0530

    Assignment:  eslint: added indentation check

--- src/mock-data/_mockcampaigndata.ts
+++ src/mock-data/_mockcampaigndata.ts
@@ -1,16 +1,16 @@
 import { ICampaignTable } from "../models/campaigntable.model";
 
 export const mockCampaignData: ICampaignTable[] = [
-    { "id": 10, "name": "Siddharth", "startDate": "3/5/2018", "endDate": "10/2/2027", "Budget": 900, "userId": 11 },
-    { "id": 11, "name": "Divavu", "startDate": "9/19/2017", "endDate": "3/9/2018", "Budget": 3000, "userId": 3 },
-    { "id": 12, "name": "Jaxspan", "startDate": "11/21/2017", "endDate": "2/21/2018", "Budget": 10000, "userId": 6 },
-    { "id": 13, "name": "Miboo", "startDate": "11/1/2017", "endDate": "6/20/2017", "Budget": 500, "userId": 7 },
-    { "id": 14, "name": "Trilith", "startDate": "8/25/2017", "endDate": "11/30/2017", "Budget": 500000, "userId": 1 },
-    { "id": 15, "name": "Layo", "startDate": "11/28/2017", "endDate": "3/10/2018", "Budget": 980, "userId": 9 },
-    { "id": 16, "name": "Photojam", "startDate": "7/25/2017", "endDate": "6/23/2017", "Budget": 1300, "userId": 3 },
-    { "id": 17, "name": "Blogtag", "startDate": "6/27/2017", "endDate": "1/15/2018", "Budget": 5200000, "userId": 2 },
-    { "id": 18, "name": "Rhyzio", "startDate": "10/13/2017", "endDate": "1/25/2018", "Budget": 272552, "userId": 4 },
-    { "id": 19, "name": "Zoomcast", "startDate": "9/6/2017", "endDate": "11/10/2017", "Budget": 301919, "userId": 8 },
-    { "id": 20, "name": "Kabir", "startDate": "3/5/2018", "endDate": "10/2/2017", "Budget": 505602, "userId": 5 },
-    { "id": 21, "name": "Varun", "startDate": "6/16/2022", "endDate": "6/16/2024", "Budget": 900, "userId": 10 },
+  { "id": 10, "name": "Siddharth", "startDate": "3/5/2018", "endDate": "10/2/2027", "Budget": 900, "userId": 11 },
+  { "id": 11, "name": "Divavu", "startDate": "9/19/2017", "endDate": "3/9/2018", "Budget": 3000, "userId": 3 },
+  { "id": 12, "name": "Jaxspan", "startDate": "11/21/2017", "endDate": "2/21/2018", "Budget": 10000, "userId": 6 },
+  { "id": 13, "name": "Miboo", "startDate": "11/1/2017", "endDate": "6/20/2017", "Budget": 500, "userId": 7 },
+  { "id": 14, "name": "Trilith", "startDate": "8/25/2017", "endDate": "11/30/2017", "Budget": 500000, "userId": 1 },
+  { "id": 15, "name": "Layo", "startDate": "11/28/2017", "endDate": "3/10/2018", "Budget": 980, "userId": 9 },
+  { "id": 16, "name": "Photojam", "startDate": "7/25/2017", "endDate": "6/23/2017", "Budget": 1300, "userId": 3 },
+  { "id": 17, "name": "Blogtag", "startDate": "6/27/2017", "endDate": "1/15/2018", "Budget": 5200000, "userId": 2 },
+  { "id": 18, "name": "Rhyzio", "startDate": "10/13/2017", "endDate": "1/25/2018", "Budget": 272552, "userId": 4 },
+  { "id": 19, "name": "Zoomcast", "startDate": "9/6/2017", "endDate": "11/10/2017", "Budget": 301919, "userId": 8 },
+  { "id": 20, "name": "Kabir", "startDate": "3/5/2018", "endDate": "10/2/2017", "Budget": 505602, "userId": 5 },
+  { "id": 21, "name": "Varun", "startDate": "6/16/2022", "endDate": "6/16/2024", "Budget": 900, "userId": 10 },
 ];
--- src/services/campaigntable.service.ts
+++ src/services/campaigntable.service.ts
@@ -4,14 +4,14 @@ import { ICampaignTable, IUserData } from "../models/campaigntable.model";
 import { rangeComparer } from "../common/utils";
 
 export const getCampaignData = () => {
-    return new Promise<ICampaignTable[]>((resolve) => {
-        const arrWithActive = mockCampaignData?.map(val => ({ ...val, isActive: rangeComparer(val?.startDate, val?.endDate) }));
-        resolve(arrWithActive);
-    });
+  return new Promise<ICampaignTable[]>((resolve) => {
+    const arrWithActive = mockCampaignData?.map(val => ({ ...val, isActive: rangeComparer(val?.startDate, val?.endDate) }));
+    resolve(arrWithActive);
+  });
 };
 
 export const getUserData = () => {
-    return http.get<IUserData[]>("https://jsonplaceholder.typicode.com/users").then(res => {
-        return res.data;
-    });
+  return http.get<IUserData[]>("https://jsonplaceholder.typicode.com/users").then(res => {
+    return res.data;
+  });
 };
