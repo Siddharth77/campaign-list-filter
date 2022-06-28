b93a07bf4cd3ec1eca41f861805724241d3de075
Siddharth Jain <siddharth.jain02@nagarro.com>
2022-06-28 13:40:34 +0530

    Assignment: fix test case

--- src/__tests__/CampaignList.test.tsx
+++ src/__tests__/CampaignList.test.tsx
@@ -2,14 +2,15 @@ import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../app/store";
 import CampaignList from "../pages/home/CampaignList";
+import '@testing-library/jest-dom/extend-expect';
 
 describe("<CampaignList />", () => {
   test("should check for CampaignList component test id defined", async () => {
-    const { queryByTestId } = render(
+    const { getByTestId } = render(
       <Provider store={store}>
         <CampaignList /> 
       </Provider>
     );
-    expect(queryByTestId).toBeDefined();
+    expect(getByTestId("campaign-list-heading")).toHaveTextContent("Campaign List Filters by Date Range and Name");
   });
 });
